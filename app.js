723508f9be9228d82c364110a14865d18df0187f
BenAhmedAmir <dragotn@gmail.com>
2020-08-19 14:56:02 +0200

    basic authentication

--- app.js
+++ app.js
@@ -34,6 +34,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    var err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+
+  }
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+  if (username === 'admin' && password === 'password') {
+    next();
+  } else {
+    var err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+app.use(auth);
+
+
+//this call is what enables us to serve static data from the public
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
